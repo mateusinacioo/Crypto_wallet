8492086a3c5d87419461f92c0ec7e79652c8c965
mateusinacioo <mateus.inacio321@gmail.com>
2023-01-16 12:34:57 -0300

    Add i18n

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,13 @@
 module ApplicationHelper
 
+    def locale(locale)
+        if I18n.locale == :en
+            "Estados Unidos"
+        else
+            "Brasil"
+        end
+    end
+
     def data_br(data_us)
         data_us.strftime("%d/%m/%Y")
     end
