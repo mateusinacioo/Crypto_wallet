518aeb2edb9c71d92c10b7ed035d08c11d1da01c
mateusinacioo <mateus.inacio321@gmail.com>
2023-01-23 23:44:08 -0300

    link to language

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    session[:curso] = "Curso de Ruby on Rails - Jackson Pires [SESSION]"
+    cookies[:curso] = "Curso de Ruby on Rails - Jackson Pires [COOKIE]"
     @meu_nome = params[:nome]
     @curso = params[:curso]
   end
